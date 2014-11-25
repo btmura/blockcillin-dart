cf7a288cf71a48d4e51fe3524572cc67e227b6e7
Brian Muramatsu <btmura@gmail.com>
2014-11-24 22:25:20 -0800

    add Block and BlockColor classes

---
+++ lib/src/block.dart
@@ -0,0 +1,16 @@
+class Block {
+
+  BlockColor color;
+
+  Block(this.color);
+}
+
+class BlockColor {
+
+  static const RED = const BlockColor._(0);
+  static const BLUE = const BlockColor._(1);
+
+  final int _value;
+
+  const BlockColor._(this._value);
+}
---
+++ test/src/block_test.dart
@@ -0,0 +1,12 @@
+import 'package:blockcillin/src/block.dart';
+import 'package:unittest/unittest.dart';
+
+main() {
+  test("Block.color", () {
+    var b = new Block(BlockColor.RED);
+    expect(b.color, equals(BlockColor.RED));
+
+    b.color = BlockColor.BLUE;
+    expect(b.color, equals(BlockColor.BLUE));
+  });
+}
