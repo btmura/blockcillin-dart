f22ca8c81628148d98ad27cd27cdab24a3ad0074
Brian Muramatsu <btmura@gmail.com>
2014-11-24 16:47:51 -0800

    serve favicon.ico using appengine context

--- bin/server.dart
+++ bin/server.dart
@@ -4,7 +4,11 @@ import 'package:appengine/appengine.dart';
 
 main() {
   runAppEngine((HttpRequest request) {
-    request.response..write('Hello, world!')
-                    ..close();
+    if (request.uri.path == "/favicon.ico") {
+     context.assets.serve();   
+    } else {
+      request.response..write('Hello, blockcillin!')
+                      ..close();
+    }
   });
 }
