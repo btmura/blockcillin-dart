e5f1fd589916a8c78bbb877964880a90db818f2f
Brian Muramatsu <btmura@gmail.com>
2014-11-25 17:46:29 -0800

    add canvas and fill it with black

--- web/index.dart
+++ web/index.dart
@@ -1,8 +1,32 @@
 import 'dart:html';
+import 'dart:web_gl' as webgl;
 
 import 'package:blockcillin/src/game.dart';
 
 main() {
   var game = new Game.withRandomBoard(3, 3);
-  querySelector("#text").text = "Hello, blockcillin!";
+
+  var canvas = querySelector("#canvas");
+  var gl = getWebGL(canvas);
+  if (gl == null) {
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(webgl.COLOR_BUFFER_BIT);
 }
+
+webgl.RenderingContext getWebGL(CanvasElement canvas) {
+  var gl = canvas.getContext("webgl");
+  if (gl != null) {
+    return gl;
+  }
+
+  gl = canvas.getContext("experimental-webgl");
+  if (gl != null) {
+    return gl;
+  }
+
+  return null;
+}
+
