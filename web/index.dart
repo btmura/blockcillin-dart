cebdfef3b5bed737f797b83b0b017885fc1cc1a9
Brian Muramatsu <btmura@gmail.com>
2014-11-25 18:37:22 -0800

    add some code to create a GL program with shaders

--- web/index.dart
+++ web/index.dart
@@ -12,6 +12,26 @@ main() {
     return;
   }
 
+  var vertexShaderSource = '''
+    void main(void) {
+      gl_Position = vec4(0.0, 0.0, 0.0, 0.0);    
+    }
+  ''';
+
+  var fragmentShaderSource = '''
+    precision mediump float;
+
+    void main(void) {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  ''';
+
+  var program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
+  if (program == null) {
+    return;
+  }
+  gl.useProgram(program);
+
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(webgl.COLOR_BUFFER_BIT);
 }
@@ -28,5 +48,48 @@ webgl.RenderingContext getWebGL(CanvasElement canvas) {
   }
 
   return null;
+}
+
+webgl.Program createProgram(webgl.RenderingContext gl, String vertexShaderSource, String fragmentShaderSource) {
+  var vertexShader = createShader(gl, webgl.VERTEX_SHADER, vertexShaderSource);
+  if (vertexShader == null) {
+    return null;
+  }
+
+  var fragmentShader = createShader(gl, webgl.FRAGMENT_SHADER, fragmentShaderSource);
+  if (fragmentShader == null) {
+    return null;
+  }
+
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  var linked = gl.getProgramParameter(program, webgl.LINK_STATUS);
+  if (!linked) {
+    var error = gl.getProgramInfoLog(program);
+    print("error linking program: $error");
+    gl.deleteProgram(program);
+    return null;
+  }
+
+  return program;
+}
+
+webgl.Shader createShader(webgl.RenderingContext gl, int type, String source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  var compiled = gl.getShaderParameter(shader, webgl.COMPILE_STATUS);
+  if (!compiled) {
+    var error = gl.getShaderInfoLog(shader);
+    print("error compiling shader: $type $error");
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
 }
 
