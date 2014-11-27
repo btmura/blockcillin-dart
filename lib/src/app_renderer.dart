af29454a4682d3e577b42b619448259130d01ad4
Brian Muramatsu <btmura@gmail.com>
2014-11-26 18:29:16 -0800

    add menu button to start new game

--- lib/src/app_renderer.dart
+++ lib/src/app_renderer.dart
@@ -14,11 +14,18 @@ class AppRenderer {
   BodyElement _body;
   DivElement _buttonBar;
   CanvasElement _canvas;
+  DivElement _newGameButton;
+
+  webgl.RenderingContext _gl;
+  int _positionAttrib;
+  webgl.Buffer _vertexBuffer;
+  webgl.Buffer _indexBuffer;
 
   AppRenderer(this.app) {
     _body = querySelector("body");
     _buttonBar = querySelector("#button-bar");
     _canvas = querySelector("#canvas");
+    _newGameButton = querySelector("#new-game-button");
   }
 
   void render() {
@@ -27,6 +34,10 @@ class AppRenderer {
 
     window.onResize.listen((event) {
       _resizeCanvas();
+    });
+
+    _newGameButton.onClick.listen((event) {
+      _draw();
     });
   }
 
@@ -45,8 +56,8 @@ class AppRenderer {
   }
 
   void _initGL() {
-    var gl = getWebGL("#canvas");
-    if (gl == null) {
+    _gl = getWebGL("#canvas");
+    if (_gl == null) {
       return;
     }
 
@@ -66,20 +77,19 @@ class AppRenderer {
       }
     ''';
 
-    var program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
+    var program = createProgram(_gl, vertexShaderSource, fragmentShaderSource);
     if (program == null) {
       return;
     }
-    gl.useProgram(program);
+    _gl.useProgram(program);
 
-    var positionAttrib = gl.getAttribLocation(program, "a_position");
-    if (positionAttrib == null) {
+    _positionAttrib = _gl.getAttribLocation(program, "a_position");
+    if (_positionAttrib == null) {
       print("positionAttrib is null");
       return;
     }
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(webgl.COLOR_BUFFER_BIT);
+    _gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     var vertexData = [
         0.0, 0.0, 0.0,
@@ -87,21 +97,27 @@ class AppRenderer {
         1.0, 0.0, 0.0,
         0.0, -1.0, 0.0,
     ];
-    webgl.Buffer vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(webgl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(webgl.ARRAY_BUFFER, new Float32List.fromList(vertexData), webgl.STATIC_DRAW);
-
-    gl.enableVertexAttribArray(positionAttrib);
-    gl.vertexAttribPointer(positionAttrib, 3, webgl.FLOAT, false, 0, 0);
+    _vertexBuffer = _gl.createBuffer();
+    _gl.bindBuffer(webgl.ARRAY_BUFFER, _vertexBuffer);
+    _gl.bufferData(webgl.ARRAY_BUFFER, new Float32List.fromList(vertexData), webgl.STATIC_DRAW);
 
     var indexData = [
         0, 1, 2,
         0, 3, 2,
     ];
-    webgl.Buffer indexBuffer = gl.createBuffer();
-    gl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(webgl.ELEMENT_ARRAY_BUFFER, new Uint16List.fromList(indexData), webgl.STATIC_DRAW);
+    _indexBuffer = _gl.createBuffer();
+    _gl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    _gl.bufferData(webgl.ELEMENT_ARRAY_BUFFER, new Uint16List.fromList(indexData), webgl.STATIC_DRAW);
+  }
 
-    gl.drawElements(webgl.TRIANGLES, 6, webgl.UNSIGNED_SHORT, 0);
+  void _draw() {
+    _gl.clear(webgl.COLOR_BUFFER_BIT);
+
+    _gl.bindBuffer(webgl.ARRAY_BUFFER, _vertexBuffer);
+    _gl.enableVertexAttribArray(_positionAttrib);
+    _gl.vertexAttribPointer(_positionAttrib, 3, webgl.FLOAT, false, 0, 0);
+
+    _gl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    _gl.drawElements(webgl.TRIANGLES, 6, webgl.UNSIGNED_SHORT, 0);
   }
 }
