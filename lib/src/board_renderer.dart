5e406161fc63330af9a94e7f14d2bd4cb57a0f68
Brian Muramatsu <btmura@gmail.com>
2014-12-06 11:58:38 -0800

    make square green

--- lib/src/board_renderer.dart
+++ lib/src/board_renderer.dart
@@ -4,9 +4,11 @@ class BoardRenderer {
 
   final webgl.RenderingContext _gl;
   final webgl.Program _program;
-  final int _positionAttrib;
+  final int _positionLocation;
+  final int _textureCoordLocation;
 
   final webgl.Buffer _vertexBuffer;
+  final webgl.Buffer _textureBuffer;
   final webgl.Buffer _indexBuffer;
 
   factory BoardRenderer(GLProgram glProgram) {
@@ -24,6 +26,22 @@ class BoardRenderer {
         ..bindBuffer(webgl.ARRAY_BUFFER, vertexBuffer)
         ..bufferData(webgl.ARRAY_BUFFER, new Float32List.fromList(vertexData), webgl.STATIC_DRAW);
 
+    var texture = gl.createTexture();
+    var green = [0, 255, 0, 255];
+    gl.bindTexture(webgl.TEXTURE_2D, texture);
+    gl.texImage2D(webgl.TEXTURE_2D, 0, webgl.RGBA, 1, 1, 0, webgl.RGBA, webgl.UNSIGNED_BYTE, new Uint8List.fromList(green));
+
+    var textureData = [
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+    ];
+    var textureBuffer = gl.createBuffer();
+    gl
+        ..bindBuffer(webgl.ARRAY_BUFFER, textureBuffer)
+        ..bufferData(webgl.ARRAY_BUFFER, new Float32List.fromList(textureData), webgl.STATIC_DRAW);
+
     var indexData = [
         0, 1, 2,
         0, 2, 3,
@@ -33,16 +51,22 @@ class BoardRenderer {
         ..bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, indexBuffer)
         ..bufferData(webgl.ELEMENT_ARRAY_BUFFER, new Uint16List.fromList(indexData), webgl.STATIC_DRAW);
 
-    return new BoardRenderer._(gl, program, glProgram.positionLocation, vertexBuffer, indexBuffer);
+
+    return new BoardRenderer._(gl, program, glProgram.positionLocation, glProgram.textureCoordLocation, vertexBuffer, textureBuffer, indexBuffer);
   }
 
-  BoardRenderer._(this._gl, this._program, this._positionAttrib, this._vertexBuffer, this._indexBuffer);
+  BoardRenderer._(this._gl, this._program, this._positionLocation, this._textureCoordLocation, this._vertexBuffer, this._textureBuffer, this._indexBuffer);
 
   void render(Board board) {
     _gl
         ..bindBuffer(webgl.ARRAY_BUFFER, _vertexBuffer)
-        ..enableVertexAttribArray(_positionAttrib)
-        ..vertexAttribPointer(_positionAttrib, 3, webgl.FLOAT, false, 0, 0);
+        ..enableVertexAttribArray(_positionLocation)
+        ..vertexAttribPointer(_positionLocation, 3, webgl.FLOAT, false, 0, 0);
+
+    _gl
+        ..bindBuffer(webgl.ARRAY_BUFFER, _textureBuffer)
+        ..enableVertexAttribArray(_textureCoordLocation)
+        ..vertexAttribPointer(_textureCoordLocation, 2, webgl.FLOAT, false, 0, 0);
 
     _gl
         ..bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, _indexBuffer)
--- lib/src/gl_program.dart
+++ lib/src/gl_program.dart
@@ -7,23 +7,31 @@ class GLProgram {
   final webgl.UniformLocation projectionMatrixLocation;
   final webgl.UniformLocation viewMatrixLocation;
   final int positionLocation;
+  final int textureCoordLocation;
 
   factory GLProgram(webgl.RenderingContext gl) {
     var vertexShaderSource = '''
       uniform mat4 u_projectionMatrix;
       uniform mat4 u_viewMatrix;
       attribute vec4 a_position;
+      attribute vec2 a_textureCoord;
+
+      varying vec2 v_textureCoord;
 
       void main(void) {
         gl_Position = u_projectionMatrix * u_viewMatrix * a_position;
+        v_textureCoord = a_textureCoord;
       }
     ''';
 
     var fragmentShaderSource = '''
       precision mediump float;
 
+      varying vec2 v_textureCoord;      
+      uniform sampler2D u_texture;
+
       void main(void) {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        gl_FragColor = texture2D(u_texture, v_textureCoord);
       }
     ''';
 
@@ -47,8 +55,13 @@ class GLProgram {
       throw new StateError("a_position not found");
     }
 
-    return new GLProgram._(gl, program, projectionMatrixLocation, viewMatrixLocation, positionLocation);
+    var textureCoordLocation = gl.getAttribLocation(program, "a_textureCoord");
+    if (textureCoordLocation == null) {
+      throw new StateError("a_textureCoord not found");
+    }
+
+    return new GLProgram._(gl, program, projectionMatrixLocation, viewMatrixLocation, positionLocation, textureCoordLocation);
   }
 
-  GLProgram._(this.gl, this.program, this.projectionMatrixLocation, this.viewMatrixLocation, this.positionLocation);
+  GLProgram._(this.gl, this.program, this.projectionMatrixLocation, this.viewMatrixLocation, this.positionLocation, this.textureCoordLocation);
 }
