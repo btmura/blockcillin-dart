8565f23870180b71ca32c553c4b5953063ccc2e5
Brian Muramatsu <btmura@gmail.com>
2014-12-13 00:13:44 -0800

    add quaternion constructor

--- lib/src/quaternion.dart
+++ lib/src/quaternion.dart
@@ -2,4 +2,17 @@ part of client;
 
 class Quaternion {
 
+  final Float32List floatList;
+
+  Quaternion(double x, double y, double z, double w) : floatList = new Float32List(4) {
+    floatList[0] = x;
+    floatList[1] = y;
+    floatList[2] = z;
+    floatList[3] = w;
+  }
+
+  double get x => floatList[0];
+  double get y => floatList[1];
+  double get z => floatList[2];
+  double get w => floatList[3];
 }
--- test/src/quaternion_test.dart
+++ test/src/quaternion_test.dart
@@ -2,5 +2,12 @@ part of test;
 
 _quaternion_tests() {
   group("quaternion", () {
+    test("Quaternion(x, y, z, w)", () {
+      var q = new Quaternion(1.0, 2.0, 3.0, 4.0);
+      expect(q.x, equals(1.0));
+      expect(q.y, equals(2.0));
+      expect(q.z, equals(3.0));
+      expect(q.w, equals(4.0));
+    });
   });
 }
