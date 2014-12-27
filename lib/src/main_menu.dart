ae8c4b021b374e646ac0d967c005ab6d7c552317
Brian Muramatsu <btmura@gmail.com>
2014-12-26 17:24:36 -0800

    remove optimization to position main menu

--- lib/src/main_menu.dart
+++ lib/src/main_menu.dart
@@ -6,21 +6,12 @@ class MainMenu {
   final ButtonElement _continueGameButton;
   final ButtonElement _newGameButton;
 
-  bool _mainMenuVisible;
-  bool _continueGameButtonVisible;
-
   MainMenu(this._mainMenu, this._continueGameButton, this._newGameButton);
 
   ElementStream<MouseEvent> get onContinueGameButtonClick => _continueGameButton.onClick;
   ElementStream<MouseEvent> get onNewGameButtonClick => _newGameButton.onClick;
 
   void set visible(bool visible) {
-    if (_mainMenuVisible == visible) {
-      return;
-    }
-
-    print("main menu visible: $visible");
-
     if (visible) {
       _show();
     } else {
@@ -29,26 +20,17 @@ class MainMenu {
   }
 
   void set continueGameButtonVisible(bool visible) {
-    if (_continueGameButtonVisible == visible) {
-      return;
-    }
-
-    print("continue game button visible: $visible");
-
     _continueGameButton.style.display = visible ? "inline-block" : "none";
-    _continueGameButtonVisible = visible;
   }
 
   void _show() {
     // Add menu first to calculate it's height before centering it.
     document.body.children.add(_mainMenu);
     _centerVertically();
-    _mainMenuVisible = true;
   }
 
   void _hide() {
     _mainMenu.remove();
-    _mainMenuVisible = false;
   }
 
   void _centerVertically() {
